64d889a52766144306be0c516f7fa270097f5085
Erick Salazar <1317428+cancerick@users.noreply.github.com>
2025-04-24 23:47:51 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import app from './app';
 
 const port = 3000;
 app.listen(port, () => {
-    return console.log(`Express is listening on port: ${port}`);
+    return console.log(`Express is listening on port: ${port} - testing`);
 });
